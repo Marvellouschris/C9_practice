3d0f77092b46d5161c807d54b50f7d97b89416da
Ekhator Marvellous <marvellousekhatorik@gmail.com>
2022-08-21 18:23:38 +0100

    Sum and average of student marks
    Doing Great things

---
+++ Sumandaverage.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//Marvellouschris
+/* Calculating the sum and averege marks of students */
+
+int main()
+{
+  int i;
+  float sum = 0, averg;
+  arr_ay[100];
+  
+  printf("Enter Marks of students: ");
+  for (i = 0; i < 100; i++);
+  {
+    scanf("%f", &arr_ay[i]);
+    sum = sum + arr_ay[i]; //added sum to the number of student
+    averg = sum/70; 
+  }
+  printf("\nMarks of student for sum are %f and average are %f", sum, averg);
+}
+  
